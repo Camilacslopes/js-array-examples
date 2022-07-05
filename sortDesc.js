b7ca3f8a7f1851c16f4824ca88538de0f9e88426
Camila Lopes <camilacslopes@gmail.com>
2022-07-05 20:06:04 +0000

    feat: Create a function to sort an array of numbers in descending order

--- sortDesc.js
+++ sortDesc.js
@@ -5,5 +5,5 @@ function sortDesc( arr ) {
   return arr.sort((a, b) => b - a)
 }
 
-console.log(sortDesc([1,3,2]))    //[3,2,1]
-console.log(sortDesc([4,2,3,1]))  //[4,3,2,1]
+console.log(sortDesc([1,3,2]))     //[3,2,1]
+console.log(sortDesc([4,2,3,1]))   //[4,3,2,1]
