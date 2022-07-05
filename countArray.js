392914cf01c6873b237a6727e6ee59eeadad1be4
Camila Lopes <camilacslopes@gmail.com>
2022-07-05 14:17:18 +0000

    feat: Create a function to count number of elements in Array

---
+++ countArray.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+
+function countArray(a) {
+    return a.length;
+}
+
+console.log(countArray([1,2,2,4]))    //4
+console.log(countArray([9,9,9]))      //3
+console.log(countArray([4,3,2,1,0]))  //5
